98891a1c61ae8b088f19bb84db72a825cd4f3fe9
Alejandro Blanco <ablanco@yaco.es>
2012-01-20 13:05:44 +0100

    New plugin for PEP8 checking

--- kate_plugins/kate_plugins.py
+++ kate_plugins/kate_plugins.py
@@ -5,3 +5,4 @@ from pyte_plugins.autocomplete import *
 from pyte_plugins.djte_plugins.class_plugins import *
 from pyte_plugins.djte_plugins.text_plugins import *
 from pyte_plugins.text_plugins import *
+from pyte_plugins.check_plugins.pep8_plugins import *
