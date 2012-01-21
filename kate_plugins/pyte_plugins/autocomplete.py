2c5fc68d5412ecdf3d122fc6eb3daa41a43843b3
Pablo Martín <goinnn@gmail.com>
2012-01-21 16:22:51 +0100

    Now create the autocompleted without shortcut

--- kate_plugins/pyte_plugins/autocomplete.py
+++ kate_plugins/pyte_plugins/autocomplete.py
@@ -13,21 +13,29 @@ class AutoCompleter(QtGui.QCompleter):
     def auto_insertText(self, text):
         self.view.insertText('.%s' % text)
 
-#import utils; utils.ipdb()
+
+def autocompleteDocument(*args, **kwargs):
+    currentDocument = kate.activeDocument()
+    print currentDocument.line(0)
+
+
+def createSignalAutocompleteDocument(view, **kwargs):
+    # https://launchpad.net/ubuntu/precise/+source/pykde4
+    # https://launchpad.net/ubuntu/precise/+source/pykde4/4:4.7.97-0ubuntu1/+files/pykde4_4.7.97.orig.tar.bz2
+    view.document().connect(view.document(),
+                        QtCore.SIGNAL("textInserted (KTextEditor::Document*, const KTextEditor::Range&)"),
+                        autocompleteDocument)
+windowInterface = kate.application.activeMainWindow()
+windowInterface.connect(windowInterface,
+                QtCore.SIGNAL('viewCreated(KTextEditor::View*)'),
+                createSignalAutocompleteDocument)
+
 
 @kate.action('autopate', shortcut='Ctrl+.', menu='Edit')
 def autoPate():
     currentDocument = kate.activeDocument()
     view = currentDocument.activeView()
     currentPosition = view.cursorPosition()
-    def update(*args, **kwargs):
-        currentDocument = kate.activeDocument()
-        print currentDocument.line(0)
-    # https://launchpad.net/ubuntu/precise/+source/pykde4
-    # https://launchpad.net/ubuntu/precise/+source/pykde4/4:4.7.97-0ubuntu1/+files/pykde4_4.7.97.orig.tar.bz2
-    currentDocument.connect(currentDocument,
-                        QtCore.SIGNAL("textInserted (KTextEditor::Document*, const KTextEditor::Range&)"),
-                        update)
     word_list = ['contrib', 'auth', 'admin', 'forms',
                  'django.forms import ModelForm']
 
