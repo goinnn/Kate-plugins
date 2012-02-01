df5c61ea56ffb0632e67c643fcb28db3d3cb0983
Pablo Martín <goinnn@gmail.com>
2012-02-01 23:56:05 +0100

    Change the version and add the changes

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*rnames):
  
 setup(
     name='Kate-plugins',
-    version="0.0.2",
+    version="0.0.3",
     description="Plugins to Kate editor to develop faster python projects, django projects and something of javascript",
     long_description=(read('README.rst') + '\n\n' + read('CHANGES')),
     author="Pablo Martin",
