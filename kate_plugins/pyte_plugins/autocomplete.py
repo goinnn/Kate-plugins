153f34062430ff9858e558de051bfc2f88858930
Pablo Martín <goinnn@gmail.com>
2012-02-25 18:34:07 +0100

    Add comments

--- kate_plugins/pyte_plugins/autocomplete.py
+++ kate_plugins/pyte_plugins/autocomplete.py
@@ -25,19 +25,28 @@ python_path = []
 PYSMELL_PREFIX = '__package____module__.'
 
 _spaces = "(?:\ |\t|\n)*"
+# "   from"
 _from = "%sfrom" % _spaces
+# "   import" 
 _import = "%simport" % _spaces
 _first_module = "(?P<firstmodule>\w+)"
 _other_module = "(?:[.](?P<othermodule>[\w.]+)?)?"
 _import_module = "(?:\w+,(?:\ |\t|\n)*)*(?P<importmodule>\w+)"
 
+# "   from   "
 _from_begin = "%s%s" % (_from, _spaces)
+# "   from   os"
 _from_first_module = "%s%s" % (_from_begin, _first_module)
+# "   from   os.path"
 _from_other_modules = "%s%s" % (_from_first_module, _other_module)
+# "   from   os.path import"
 _from_import = "%s%s%s" % (_from_other_modules, _spaces, _import)
+# "   from   os.path import join"
 _from_complete = "%s%s%s?" % (_from_import, _spaces, _import_module)
 
+# "   import" 
 _import_begin = "%s" % _import
+# "   import   os" 
 _import_complete = "%s%s%s?" % (_import_begin, _spaces, _import_module)
 
 from_first_module = re.compile(_from_first_module + "?$")
