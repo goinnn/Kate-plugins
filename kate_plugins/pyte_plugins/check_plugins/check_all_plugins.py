6fcbca5ef9423dd1ee763fa1be98bcb13068b984
Pablo Martín <goinnn@gmail.com>
2012-03-04 20:59:20 +0100

    Removed the old popups

--- kate_plugins/pyte_plugins/check_plugins/check_all_plugins.py
+++ kate_plugins/pyte_plugins/check_plugins/check_all_plugins.py
@@ -1,6 +1,6 @@
 import kate
 
-from PyQt4 import QtCore
+from PyQt4 import QtCore, QtGui
 
 from kate_settings_plugins import KATE_ACTIONS, CHECKALL_TO_SAVE
 
@@ -11,6 +11,19 @@ def clearMarksOfError(doc, mark_iface):
             mark_iface.removeMark(line, mark_iface.Error)
 
 
+def hideOldPopUps():
+    mainWindow = kate.mainWindow()
+    popups = kate.gui.TimeoutPassivePopup.popups.get(mainWindow, []) or []
+    for popup in popups:
+        popup.timer.stop()
+        popup.hide()
+        popup.setFixedHeight(0)
+        popup.adjustSize()
+        popup.originalHeight = popup.height()
+        popup.offsetBottom = 0
+        popup.move(0, 0)
+
+
 @kate.action(**KATE_ACTIONS['checkAll'])
 def checkAll(doc=None, excludes=None, exclude_all=False):
     from pyte_plugins.check_plugins.parse_plugins import parseCode
@@ -19,6 +32,7 @@ def checkAll(doc=None, excludes=None, exclude_all=False):
         currentDoc = doc or kate.activeDocument()
         mark_iface = currentDoc.markInterface()
         clearMarksOfError(currentDoc, mark_iface)
+        hideOldPopUps()
         show_popup = not excludes
         if not exclude_all:
             if not 'parseCode' in excludes:
