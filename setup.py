722026f924ce421511b318fdffbae5433908aa91
Pablo Martín <goinnn@gmail.com>
2013-03-11 10:13:27 +0100

    New release

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ def read(*rnames):
 
 setup(
     name='Kate-plugins',
-    version="0.2.0",
+    version="0.2.1",
     description="Plugins to Kate editor to develop faster python projects, django projects and something of javascript",
     long_description=(read('README.rst') + '\n\n' + read('CHANGES.rst')),
     author="Pablo Martin",
